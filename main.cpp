5e9a01b05424a03f0c2fb6901a8ee77bd4116869
Steve Hamblett <steve.hamblett@linux.com>
2017-05-10 08:25:41 +0000

    Latest

--- main.cpp
+++ main.cpp
@@ -8,12 +8,19 @@
 #include "src/embeddedvm.h"
 
 /**
- * Main entry point, initializes the VM and runs the supplied script
- * with the supplied VM options.
+ * Main entry point, initializes the VM and runs the supplied script.
+ * VM options are preset, checked mode is on, the package root is the current
+ * directory. Trace loading is also on to aid in package path debugging.
+ * The supplied Dart script must of course have a main entry point.
  */
 int main(int argc, const char** argv) {
 
-	std::cout << "Hello from the Dart Embedded VM test" << std::endl;
+	if ( argc == 1) {
+		std::cout << "Dart Embedded VM test - please supply a dart script" << std::endl;
+		return -1;
+	}
+	std::string script(argv[1]);
+	std::cout << "Hello from the Dart Embedded VM test, running script " << script << std::endl;
 	return 0;
 }
 
